c2d4e4b22b862f63d90b4717a825387133c857ec
1016281633@qq.com <jiapeng@picooc.com>
2019-03-25 16:40:16 +0800

    修改user-agent

--- src/main/java/com/yishi/util/HttpRequest.java
+++ src/main/java/com/yishi/util/HttpRequest.java
@@ -31,8 +31,10 @@ public class HttpRequest {
             connection.setRequestProperty("accept", "*/*");
             connection.setRequestProperty("connection", "Keep-Alive");
             connection.setRequestProperty("charset", "utf-8");
-            connection.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			// connection.setRequestProperty("user-agent",
+			// "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			connection.setRequestProperty("user-agent",
+					"WeiboDongman/7.6.5-Beta3 (com.guoke.weibodongman; build:201903151902; iOS 11.2.6) Alamofire/4.8.0");
             // 建立实际的连接
             connection.connect();
             // 获取所有响应头字段
@@ -85,8 +87,10 @@ public class HttpRequest {
             // 设置通用的请求属性
             conn.setRequestProperty("accept", "*/*");
             conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			conn.setRequestProperty("user-agent",
+					"WeiboDongman/7.6.5-Beta3 (com.guoke.weibodongman; build:201903151902; iOS 11.2.6) Alamofire/4.8.0");
+			// conn.setRequestProperty("user-agent",
+			// "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
